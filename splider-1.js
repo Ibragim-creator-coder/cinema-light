abde03c37054a952cb25609c081ee0b93e04c9b7
Ibragim-creator-coder <ibragimshamsiev06@gmail.com>
2026-02-24 22:01:52 +0500

    Добавил свой первый проект

---
+++ splider-1.js
@@ -0,0 +1,12 @@
+var splide = new Splide( '.splide', {
+  perPage: 2,
+  rewind: true, 
+  gap: '10px',
+  pagination: false,
+  autoplay: true,
+  interval: 1000,
+  pauseOnFocus: true,
+
+} );
+
+splide.mount();
