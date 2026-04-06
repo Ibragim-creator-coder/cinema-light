03d476fcd64ab726d2ad4fe3252e8a4cc4404242
Ibragim-creator-coder <ibragimshamsiev06@gmail.com>
2026-04-06 20:25:10 +0500

    changes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var splide = new Splide( '.splide', {
   perPage: 3,
   rewind : true,
+  type: 'loop',
+  autoplay: true,
+  interval: 1000,
+  pagination: true,
+  pauseOnHover: true,
+  pauseOnFocus: true,
   gap:20,
+  pagination: false,
 } );
 
 splide.mount();
-
-
-
