77a0eaa14408a9148592452999fa93f30084f9f7
Ibragim-creator-coder <ibragimshamsiev06@gmail.com>
2026-06-05 18:26:55 +0500

    .

--- index.js
+++ index.js
@@ -1,27 +1,82 @@
-var splide = new Splide( '.splide', {
-  perPage: 3,
-  rewind : true,
-  type: 'loop',
-  autoplay: true,
-  interval: 1000,
-  pauseOnHover: true,
-  pauseOnFocus: true,
-  gap:20,
-  pagination: false,
-} );
+document.addEventListener('DOMContentLoaded', () => {
 
-splide.mount();
+  const splide = document.querySelector('.splide');
 
-var splide1 = new Splide( '.splide-1', {
-  perPage: 3,
-  rewind : true,
-  type: 'loop',
-  autoplay: true,
-  interval: 1000,
-  pauseOnHover: true,
-  pauseOnFocus: true,
-  gap:20,
-  pagination: false,
-} );
+  if (splide) {
+    new Splide('.splide', {
+      perPage: 3,
+      rewind: true,
+      type: 'loop',
+      autoplay: true,
+      interval: 1000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      gap: 20,
+      pagination: false,
+    }).mount();
+  }
 
-splide1.mount();
+  const splide1 = document.querySelector('.splide-1');
+
+  if (splide1) {
+    new Splide('.splide-1', {
+      perPage: 3,
+      rewind: true,
+      type: 'loop',
+      autoplay: true,
+      interval: 1000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      gap: 20,
+      pagination: false,
+    }).mount();
+  }
+
+  const splide2 = document.querySelector('.splide-2');
+
+  if (splide2) {
+    new Splide('.splide-2', {
+      perPage: 3,
+      rewind: true,
+      type: 'loop',
+      autoplay: true,
+      interval: 1000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      gap: 20,
+      pagination: false,
+    }).mount();
+  }
+
+  const splide3 = document.querySelector('.splide-3');
+
+  if (splide3) {
+    new Splide('.splide-3', {
+      perPage: 3,
+      rewind: true,
+      type: 'loop',
+      autoplay: true,
+      interval: 1000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      gap: 20,
+      pagination: false,
+    }).mount();
+  }
+
+  const splide4 = document.querySelector('.splide-4');
+
+  if (splide4) {
+    new Splide('.splide-4', {
+      perPage: 3,
+      rewind: true,
+      type: 'loop',
+      autoplay: true,
+      interval: 1000,
+      pauseOnHover: true,
+      pauseOnFocus: true,
+      gap: 20,
+      pagination: false,
+    }).mount();
+  }
+});
